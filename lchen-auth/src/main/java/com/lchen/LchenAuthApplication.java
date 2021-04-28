0165419d4292f481435e9a339eeb7e48d583ccbc
liaoxunchuan <1287374083@qq.com>
2021-04-28 11:47:57 +0800

    自动装配优化

--- lchen-auth/src/main/java/com/lchen/LchenAuthApplication.java
+++ lchen-auth/src/main/java/com/lchen/LchenAuthApplication.java
@@ -1,12 +1,9 @@
 package com.lchen;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.lchen.common.swagger.annotation.EnableCustomSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -14,11 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *
  * @author lchen
  */
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        DruidDataSourceAutoConfigure.class ,
-        HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)//自动装配去掉数据源配置
 @EnableFeignClients
 @EnableCustomSwagger2
 public class LchenAuthApplication {
--- lchen-gateway/src/main/java/com/lchen/gateway/GatewayApplication.java
+++ lchen-gateway/src/main/java/com/lchen/gateway/GatewayApplication.java
@@ -1,19 +1,13 @@
 package com.lchen.gateway;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,
-        DruidDataSourceAutoConfigure.class ,
-        HibernateJpaAutoConfiguration.class})
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class}) //自动装配去掉数据源配置
 @EnableDiscoveryClient
 @ComponentScan({"com.lchen.common.redis", "com.lchen.gateway"})
 public class GatewayApplication {
