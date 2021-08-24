2d682840c943f8df8965d5c2ac287bfc0a70545c
liaoxunchuan <1287374083@qq.com>
2021-08-24 17:16:24 +0800

    线程池修改

--- lchen-common/lchen-common-core/src/main/java/com/lchen/common/core/server/ThreadPoolService.java
+++ lchen-common/lchen-common-core/src/main/java/com/lchen/common/core/server/ThreadPoolService.java
@@ -2,6 +2,10 @@ package com.lchen.common.core.server;
 
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
 
 /**
@@ -25,36 +29,58 @@ public class ThreadPoolService {
      * handler         拒绝策略类,当线程池数量达到上线并且workQueue队列长度达到上限时就需要对到来的任务做拒绝处理
      */
     private static ExecutorService service = new ThreadPoolExecutor(
-            4,
-            8,
-            0L,
-            TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<>(4),
+            3,
+            3,
+            10,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(),
             namedThreadFactory,
             new ThreadPoolExecutor.AbortPolicy()
     );
 
-    /**
-     * 获取线程池
-     * @return
-     */
-    public static ExecutorService getEs(){
-        return service;
-    }
 
-
-    /**
-     * 使用线程池创建线程并异步执行任务
-     * @param runnable 任务
-     */
-    public static void newTask(Runnable runnable){
-        service.execute(runnable);
+    private static void shutdown(ExecutorService executorService) {
+        // 第一步：使新任务无法提交
+        executorService.shutdown();
+        try {
+            // 第二步：等待未完成任务结束
+            if(!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                // 第三步：取消当前执行的任务
+                executorService.shutdownNow();
+                // 第四步：等待任务取消的响应
+                if(!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Thread pool did not terminate");
+                }
+            }
+        } catch(InterruptedException ie) {
+            // 第五步：出现异常后，重新取消当前执行的任务
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt(); // 设置本线程中断状态
+        }
     }
 
 
     public static void main(String[] args) {
-        for (int i = 0; i < 12; i++){
-            newTask(() -> System.out.println(Thread.currentThread().getName() + " is running .."));
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++){
+            list.add(i);
         }
+        try {
+            list.forEach(integer -> {
+                service.execute(() -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("i", integer);
+                    if (integer % 2 ==0){
+                        System.out.println(Thread.currentThread().getName() + Thread.currentThread().getId() + "-双数:"+ map.get("i").toString());
+                    }else {
+                        System.out.println(Thread.currentThread().getName() + Thread.currentThread().getId() + "-单数:"+ map.get("i").toString());
+                    }
+                });
+            });
+        }finally {
+            System.out.println("关闭....");
+            shutdown(service);
+        }
+
     }
 }
